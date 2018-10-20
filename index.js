eedf86c1d7d91082f0024c938d11cdf159ff457f
Aimeri Baddouh <aimeri@fashionsloth.slothcrew.com>
2018-10-20 14:31:54 -0400

    add eslint

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const dotenv = require("dotenv").config();
-const cloudinaryURI = process.env.CLOUDINARY_URL;
-const cloudinary = require("cloudinary");
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv').config()
+const cloudinaryURI = process.env.CLOUDINARY_URL
+const cloudinary = require('cloudinary')
 cloudinary.config({
-  cloudinaryURI
-});
+    cloudinaryURI
+})
 
 
 
 //request();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 const request = async () => {
-  console.log('2')
-  const pictures = await cloudinary.v2.api.resources({
-    type: "upload",
-    prefix: "slothcrewnoir/",
-    context: true,
-    max_results: 100
-  });
+    console.log('2')
+    const pictures = await cloudinary.v2.api.resources({
+        type: 'upload',
+        prefix: 'slothcrewnoir/',
+        context: true,
+        max_results: 100
+    })
 
-  const picture = await pictures.resources;
-  console.log(picture)
-  return picture
-};
+    const picture = await pictures.resources
+    console.log(picture)
+    return picture
+}
 
-app.get('/', function(req, res) {
-  
-  
-  
-  request().then((data) => {let rendered = res.render('pages/index',{
-    pictures: data})
-  })
-  //res.render('pages/index',);
-});
-
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
+app.get('/', function (req, res) {
 
 
-app.listen(8900, function() {
-  console.log("listening");
-});
 
+    request().then((data) => {
+        let rendered = res.render('pages/index', {
+            pictures: data
+        })
+    })
+    //res.render('pages/index',);
+})
+
+app.get('/about', function (req, res) {
+    res.render('pages/about')
+})
+
+
+app.listen(8900, function () {
+    console.log('listening')
+})
